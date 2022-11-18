fb26f7ed9a18feb6d0e91cd9cf22d74303df0cd4
revers3ntropy <josephcoppin@gmail.com>
2022-11-18 12:38:58 +0000

    Testing asm file

---
+++ test.asm
@@ -0,0 +1,66 @@
+section .data
+    n dd 2
+      dd 1
+section .text
+
+print_fraction:
+    pop rbx
+
+    pop rax
+
+    push rbx
+
+    push rax
+    push 1
+    call print
+
+    add rax, 0
+    push rax
+    push 1
+    call print
+
+    ret
+
+print:
+    pop rbx
+
+    pop rdx
+    pop rsi
+    mov rax, '0'
+    add [rsi], rax
+    mov rax, 1
+    mov rdi, 1
+
+    push rbx
+
+    syscall
+    ret
+
+exit:
+    mov rax, 60
+    mov rdi, 0
+    syscall
+
+print_stack:
+    pop rdi
+    pop rax
+
+    push rdi
+    push rax
+    push 1
+
+    call print
+
+    ;cmp rsp, rbp
+    ;jg print_stack
+    ret
+
+global main
+global _start
+main:
+_start:
+
+    push n
+    call print_fraction
+    ;call print_stack
+    call exit
