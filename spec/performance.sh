0c53fd281ce1a0f823cf4bdb76a9159c506380f3
revers3ntropy <josephcoppin@gmail.com>
2022-12-09 18:20:45 +0000

    extract performance comparison to C++ into function in tests

--- spec/performance.sh
+++ spec/performance.sh
@@ -1,6 +1,6 @@
 describe 'Performance'
 
-oxy_perf_code='
+perf_test_comp_cpp '49999995000000' '
     var sum = 0;
     const n = 10000000;
     var i = 0;
@@ -12,58 +12,21 @@ oxy_perf_code='
         i = i + 1;
     };
     print_int(sum);
-'
+' '
+    #include <iostream>
 
-expect '49999995000000' "$oxy_perf_code"
+    int main () {
+        long sum = 0;
+        int n = 10000000;
+        int i = 0;
+        while (true) {
+            if (i >= n) {
+                break;
+            }
 
-perf_timer_start=$(date +"%s.%3N")
-
-errors=$({
-    # in.oxy already has code in
-  ./target/release/oxynium -o=test-out --std std.asm in.oxy
-} 2>&1 > /dev/null)
-
-if [ "$errors" != "" ]; then
-    test_failed 'Oxy perf test' 'no errors' "$errors"
-fi
-
-echo "Oxy Compilation: $(echo "$(date +"%s.%3N") - $perf_timer_start" | bc -l)"
-perf_timer_start=$(date +"%s.%3N")
-
-./test-out > /dev/null
-
-echo "Oxy Execution: $(echo "$(date +"%s.%3N") - $perf_timer_start" | bc -l)"
-perf_timer_start=$(date +"%s.%3N")
-
-echo '
-#include <iostream>
-
-int main () {
-    long sum = 0;
-    int n = 10000000;
-    int i = 0;
-    while (true) {
-        if (i >= n) {
-            break;
+            sum = sum + i;
+            i = i + 1;
         }
-
-        sum = sum + i;
-        i = i + 1;
+        std::cout << sum;
     }
-    std::cout << sum;
-}
-' > out.cpp
-
-gcc out.cpp -lstdc++ -O3
-
-echo "C++ Compilation: $(echo "$(date +"%s.%3N") - $perf_timer_start" | bc -l)"
-perf_timer_start=$(date +"%s.%3N")
-
-cpp_out=$(./a.out)
-
-echo "C++ Execution: $(echo "$(date +"%s.%3N") - $perf_timer_start" | bc -l)"
-perf_timer_start=$(date +"%s.%3N")
-
-if [ "$cpp_out" != "49999995000000" ]; then
-    test_failed "C++ performance code" "49999995000000" "$cpp_out"
-fi
+'
