bb18fb06ffc250c6d3b331f433db91f270edd469
revers3ntropy <josephcoppin@gmail.com>
2022-12-20 17:46:26 +0000

    Tests for recursive function calls

--- spec/functions.sh
+++ spec/functions.sh
@@ -393,3 +393,26 @@ expect_err 'IoError' '
 expect_err 'SyntaxError' '
     extern fn f() {}
 '
+
+
+describe 'Recursion'
+
+expect '55' '
+    fn fib(n: Int): Int {
+        if n <= 1 {
+            return n;
+        };
+        return fib(n - 1) + fib(n - 2);
+    };
+    print(fib(10).str());
+'
+expect '0123456789' '
+    fn a(i = 0) {
+        if i > 9 {
+            return
+        }
+        print(i.str());
+        a(i + 1);
+    }
+    a()
+'
