f5e2cf1d429647dcf949c1f2a8785d26e2be01fb
revers3ntropy <josephcoppin@gmail.com>
2023-01-05 21:31:16 +0000

    update Cargo.toml

--- test/spec/std/Char.sh
+++ test/spec/std/Char.sh
@@ -23,3 +23,12 @@ expect_expr_bool 'false' '"a".at(0) != "a".at(0)'
 expect_expr_bool 'true'  '"a".at(0) != "b".at(0)'
 expect_expr_bool 'false' '"💖".at(0) != "💖".at(0)'
 expect_expr_bool 'false' '"🇨🇦".at(0) != "🇦".at(0)'
+
+
+describe 'fn Char.from_int'
+
+expect_expr_int 'a' 'Char.from_int(97)'
+expect_expr_int 'A' 'Char.from_int(65)'
+expect_expr_int '0' 'Char.from_int(48)'
+expect_expr_int ' ' 'Char.from_int(32)'
+expect_expr_int '💖' 'Char.from_int(128150)'
