b37e04ff9a83e12825e67ec1d7ae49dfd1c51a9f
revers3ntropy <josephcoppin@gmail.com>
2023-01-30 19:35:58 +0000

    [0.2.6] `Result` class added to std

--- test/spec/std/Result.sh
+++ test/spec/std/Result.sh
@@ -1,1 +1,24 @@
 describe 'class Result'
+
+expect '1,2,true,false' '
+    func main () {
+        print(Result.ok!<Int, Int>(1).unwrap().str());
+        print(",");
+        print(Result.ok!<Int, Str>(2).unwrap().str());
+        print(",");
+        print(Result.ok!<Int, Str>(2).is_ok().str());
+        print(",");
+        print(Result.ok!<Int, Str>(2).is_err().str());
+    }
+'
+expect '1,hi,false,true' '
+    func main () {
+        print(Result.err!<Int, Int>(1).error.unwrap().str());
+        print(",");
+        print(Result.err!<Int, Str>("hi").error.unwrap().str());
+        print(",");
+        print(Result.err!<Int, Str>("").is_ok().str());
+        print(",");
+        print(Result.err!<Int, Str>("").is_err().str());
+    }
+'
