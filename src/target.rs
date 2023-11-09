0acf0bd0fce8dc3e358df7f6ccba00d97af08e91
revers3ntropy <josephcoppin@gmail.com>
2023-11-09 13:57:40 +0000

    default to compilation target used when building the compiler

--- src/target.rs
+++ src/target.rs
@@ -5,10 +5,21 @@ pub enum Target {
 }
 
 impl Target {
+    #[cfg(target_os = "macos")]
+    pub fn current() -> Self {
+        Target::MACOS
+    }
+
+    #[cfg(target_os = "linux")]
+    pub fn current() -> Self {
+        Target::X86_64Linux
+    }
+
     pub fn from_str(s: String) -> Self {
         match s.as_str() {
             "macos" => Target::MACOS,
-            "" | "x86" | "linux" | "x86_64" => Target::X86_64Linux,
+            "x86" | "linux" | "x86_64" => Target::X86_64Linux,
+            "" => Target::current(),
             _ => {
                 eprintln!("Unknown target: {}", s);
                 Target::X86_64Linux
