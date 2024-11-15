17bc049cc4c4200933f2a6b21c313d763e02ac98
revers3ntropy <josephcoppin@gmail.com>
2024-11-15 20:17:01 +0000

    tests for `#unchecked_cast`

---
+++ test/spec/std/#unchecked_cast.sh
@@ -0,0 +1,32 @@
+describe 'macro #unchecked_cast'
+
+expect 'Void' 'print(typeof #unchecked_cast Void "")'
+expect 'Int' 'print(typeof #unchecked_cast Int, "")'
+expect 'Result<Str, Void>' '
+    print(typeof #unchecked_cast(Result<Str, Void>, Option.ok!<Int, Int>(1)))
+'
+expect 'Result<Str, Option<Int>>' '
+    print(typeof #unchecked_cast Result<Str, Option<Int>> 2)
+'
+expect 'false' '
+    def unchecked_cast() Bool {
+        return #unchecked_cast Bool 0
+    }
+    print(unchecked_cast().Str())
+'
+
+expect_err 'SyntaxError' '#unchecked_cast 1'
+expect_err 'SyntaxError' '#unchecked_cast Int'
+expect_err 'SyntaxError' '#unchecked_cast(1)'
+expect_err 'SyntaxError' '#unchecked_cast()'
+expect_err 'SyntaxError' '#unchecked_cast'
+expect_err 'SyntaxError' '#unchecked_cast Void, "")'
+expect_err 'SyntaxError' '#unchecked_cast("", 1)'
+expect_err 'SyntaxError' '#unchecked_cast(2, 1)'
+expect_err 'UnknownSymbol' '#unchecked_cast(Fish, 1)'
+expect_err 'SyntaxError' '
+    def main () {
+        let s = "";
+        #unchecked_cast(s);
+    }
+'
