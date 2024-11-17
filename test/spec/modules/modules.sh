16bfaa98cd6c15d0dceca02ee8c8345325e433d3
revers3ntropy <josephcoppin@gmail.com>
2024-11-17 22:03:12 +0000

    improve module tests

--- test/spec/modules/modules.sh
+++ test/spec/modules/modules.sh
@@ -4,3 +4,8 @@ expect 'public' '
     #import "my_module.oxy"
     my_module.do_something_public()
 '
+
+expect_err 'TypeError' '
+    #import "my_module.oxy"
+    my_module.do_something_private()
+'
