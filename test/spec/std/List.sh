a0900e5f94cd06f389334c98576bfa2ebb31980c
revers3ntropy <josephcoppin@gmail.com>
2024-12-07 22:44:59 +0000

    [0.2.23] add capacity to `List`
    introduce new attribute `List.capacity`
    
    also added and tests `List.with_capacity`, `List.reallocate`, `List.concat`, `List.clone`

--- test/spec/std/List.sh
+++ test/spec/std/List.sh
@@ -4,18 +4,22 @@ expect '732truefalsetrue' '
     def main () {
     	let l = new List<Int> {
     		head: Ptr.make!<Int>(7),
-    		size: 8
-    	};
-    	l.push(2);
-    	l.push(3);
-    	print(l.at(0).unwrap().Str());
-    	print(l.at(2).unwrap().Str());
-    	print(l.at(1).unwrap().Str());
-    	print(l.at(1).is_some.Str());
-    	print(l.at(3).is_some.Str());
-    	print(l.at(-1).is_some.Str());
+    		length: 1,
+    		capacity: 8
+    	}
+    	l.push(2)
+    	l.push(3)
+    	print(l.at(0).unwrap().Str())
+    	print(l.at(2).unwrap().Str())
+    	print(l.at(1).unwrap().Str())
+    	print(l.at(1).is_some.Str())
+    	print(l.at(3).is_some.Str())
+    	print(l.at(-1).is_some.Str())
     }
 '
+
+describe 'def List.empty, def List.push'
+
 expect '02' '
     def main () {
     	let l = List.empty!<Int>();
@@ -28,40 +32,149 @@ expect '02' '
 expect '26' '
     def main () {
     	let l = List.empty!<Int>()
-    	l.push(2, 6)
+    	l.push(2)
+    	l.push(2)
+    	l.push(2)
+    	l.push(2)
+    	l.push(2)
+    	l.push(2)
     	print(l.at(0).unwrap().Str())
     	print(l.len().Str())
     }
 '
 
 
-describe 'List.set_at'
+describe 'def List.with_capacity'
 
-expect 'false,false,false,2,true,1,false,true,3,false' '
+expect '0 0 0 1 1 64 2 2 64 0 0 8 1 1 8 2 16 0 2 1 8' '
+    def main () {
+        let mut l = List.with_capacity!<Int>(0)
+        print(l.length.Str(), " ")
+        print(l.len().Str(), " ")
+        print(l.capacity.Str(), " ")
+
+        l.push(2)
+        print(l.length.Str(), " ")
+        print(l.len().Str(), " ")
+        print(l.capacity.Str(), " ")
+
+        l.push(3)
+        print(l.length.Str(), " ")
+        print(l.len().Str(), " ")
+        print(l.capacity.Str(), " ")
+
+        l = List.with_capacity!<Int>(8)
+        print(l.length.Str(), " ")
+        print(l.len().Str(), " ")
+        print(l.capacity.Str(), " ")
+
+        l.push(67)
+        print(l.length.Str(), " ")
+        print(l.len().Str(), " ")
+        print(l.capacity.Str(), " ")
+
+        l.push(67)
+        print(l.length.Str(), " ")
+        print(l.capacity.Str(), " ")
+
+        l = List.with_capacity!<Int>(2)
+        print(l.length.Str(), " ")
+        print(l.capacity.Str(), " ")
+        // when we push to a buffer that has size but the size
+        // is not enough, the buffer will be reallocated to the required size
+        l.push(1)
+        print(l.length.Str(), " ")
+        print(l.capacity.Str(), "")
+    }
+'
+
+
+describe 'def List.clone'
+
+expect '3 11 12 2 11 14 128' '
+    def main () {
+        let mut l = List.empty!<Int>()
+        l.push(11)
+        let mut l2 = l.clone()
+        l.push(12)
+        l.push(13)
+        l2.push(14)
+        print(l.length.Str(), " ")
+        print(l.at_raw(0).Str(), " ")
+        print(l.at_raw(1).Str(), " ")
+        // check arrays are decoupled
+        print(l2.length.Str(), " ")
+        print(l2.at_raw(0).Str(), " ")
+        print(l2.at_raw(1).Str(), " ")
+
+        // check cloned arrays get the same capacity
+        l = List.with_capacity!<Int>(128)
+        print(l.clone().capacity.Str(), "")
+    }
+'
+
+
+describe 'def List.concat'
+
+expect '2 3 5 2 3 6 3 3 6 2 3 4 5 6 7' '
+    def main () {
+        let l1 = List.empty!<Int>()
+        l1.push(2)
+        l1.push(3)
+
+        let l2 = List.empty!<Int>()
+        l2.push(4)
+        l2.push(5)
+        l2.push(6)
+
+        // create new list with contents of l1 and l2
+        let l = l1.concat(l2)
+
+        print(l1.length.Str(), " ")
+        print(l2.length.Str(), " ")
+        print(l.length.Str(), " ")
+
+        l.push(7)
+
+        print(l1.length.Str(), " ")
+        print(l2.length.Str(), " ")
+        print(l.length.Str(), " ")
+
+        l1.push(0)
+
+        print(l1.length.Str(), " ")
+        print(l2.length.Str(), " ")
+        print(l.length.Str(), " ")
+
+        print(l.at_raw(0).Str(), " ")
+        print(l.at_raw(1).Str(), " ")
+        print(l.at_raw(2).Str(), " ")
+        print(l.at_raw(3).Str(), " ")
+        print(l.at_raw(4).Str(), " ")
+        print(l.at_raw(5).Str(), "")
+    }
+'
+
+
+describe 'def List.set_at'
+
+expect 'false false false 2 true 1 false true false 3 false' '
     def main () {
     	let l = List.empty!<Int>()
     	// cannot set anything in empty list
-    	print(l.set_at(0, 2).ok.Str())
-    	print(",")
-    	print(l.set_at(1, 2).ok.Str())
-    	print(",")
-    	print(l.set_at(-1, 2).ok.Str())
+    	print(l.set_at(0, 2).ok.Str(), " ")
+    	print(l.set_at(1, 2).ok.Str(), " ")
+    	print(l.set_at(-1, 2).ok.Str(), " ")
 
     	l.push(2)
 
-    	print(",")
-    	print(l.at(0).unwrap().Str())
-    	print(",")
-    	print(l.set_at(0, 1).ok.Str())
-    	print(",")
-    	print(l.at(0).unwrap().Str())
-    	print(",")
-    	print(l.set_at(1, 1).ok.Str())
-    	print(",")
-    	print(l.set_at(-1, 3).ok.Str())
-      print(",")
-      print(l.at(0).unwrap().Str())
-    	print(",")
-    	print(l.set_at(10, 1).ok.Str())
+    	print(l.at(0).unwrap().Str(), " ")
+    	print(l.set_at(0, 1).ok.Str(), " ")
+    	print(l.at(0).unwrap().Str(), " ")
+    	print(l.set_at(1, 1).ok.Str(), " ")
+    	print(l.set_at(-1, 3).ok.Str(), " ")
+        print(l.set_at(-2, 3).ok.Str(), " ")
+        print(l.at(0).unwrap().Str(), " ")
+    	print(l.set_at(10, 1).ok.Str(), "")
     }
 '
