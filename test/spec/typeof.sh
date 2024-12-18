ed37a73e3b69f396298ec490a09ab62fd81577e7
revers3ntropy <josephcoppin@gmail.com>
2024-12-18 22:15:26 +0000

    update typeof tests

--- test/spec/typeof.sh
+++ test/spec/typeof.sh
@@ -1,32 +1,21 @@
 describe 'typeof'
 
-expect 'Str Str Int Void Type Type Type Type C Bool Str Str'  '
-    print(typeof("abc"));
-    print(" ");
-    print(typeof "abc");
-    print(" ");
-    print(typeof 2);
-    print(" ");
-    print(typeof new Void);
-    print(" ");
-    print(typeof Void);
-    print(" ");
-    print(typeof Int);
-    print(" ");
-    print(typeof Str);
-    print(" ");
+expect 'Str Str Int Void Void Int Str C C Bool Str Str'  '
+    print(typeof("abc"), " ")
+    print(typeof "abc", " ")
+    print(typeof 2, " ")
+    print(typeof new Void, " ")
+    print(typeof Void, " ")
+    print(typeof Int, " ")
+    print(typeof Str, " ")
 
-    class C;
-    print(typeof C);
-    print(" ");
-    print(typeof new C);
-    print(" ");
+    class C
+    print(typeof C, " ")
+    print(typeof new C, " ")
 
-    print(typeof true);
-    print(" ");
-    print(typeof typeof Bool);
-    print(" ");
-    print(typeof typeof typeof new Void);
+    print(typeof true, " ")
+    print(typeof typeof Bool, " ")
+    print(typeof typeof typeof new Void)
 '
 expect_err 'UnknownSymbol' 'print(typeof Type)'
 expect 'Fn a() Void,Void' '
@@ -51,18 +40,15 @@ expect_err 'SyntaxError' 'print(typeof if true {})'
 expect 'Fn a() Void' 'print(typeof def a() {})'
 expect 'Fn fn@in.oxy#14() Void' 'print(typeof fn () {})'
 
-expect 'T,Type,Int,Str' '
+expect 'T,T,Int,Str' '
     def a <T> (a: T) T {
-        print(typeof a);
-        print(",");
-        print(typeof T);
-        print(",");
+        print(typeof a, ",")
+        print(typeof T, ",")
         return a
     }
     def main () {
-        let int = a!<Int>(1);
-        print(typeof int);
-        print(",");
-        print(typeof a!<Str>("Hi"));
+        let int = a!<Int>(1)
+        print(typeof int, ",")
+        print(typeof a!<Str>("Hi"))
     }
 '
