a31482c71c22ee3c9eb92768a816c9ca196139ea
josephcoppin <joseph.coppin@gmail.com>
2025-12-13 15:54:20 +0000

    fix infinite recursion issue when resolving generics

--- src/types/generic.rs
+++ src/types/generic.rs
@@ -4,6 +4,7 @@ use crate::types::Type;
 use crate::util::{mut_rc, MutRc};
 use std::collections::HashMap;
 use std::fmt;
+use std::ops::Deref;
 
 #[derive(Clone)]
 pub struct GenericType {
@@ -47,12 +48,21 @@ impl Type for GenericType {
         match generics.get(self_id) {
             None => format!("{}", self.identifier.clone().literal.unwrap()),
             Some(concrete_type) => {
-                if format!("{:p}", concrete_type.as_ptr()) == format!("{:p}", self) {
+                let concrete_type = concrete_type.borrow();
+
+                // SUPER HACKY!
+                // I can't find a way to extract the raw pointer as a string :(
+
+                // "Pointer { addr: 0xA, metadata: DynMetadata(0x0) }"
+                let concrete_type_ptr_fmt = format!("{:p}", concrete_type.deref()).to_owned();
+                // "0xA"
+                let self_ptr_fmt = format!("{:p}", self);
+                // where "0xA" will match if we are looking at ourselves here
+                if concrete_type_ptr_fmt.contains(self_ptr_fmt.as_str()) {
                     // avoid circular loop when the generic is
                     // the same as the concrete type (not yet concreted)
                     format!("{}", self.identifier.clone().literal.unwrap())
                 } else {
-                    let concrete_type = concrete_type.borrow();
                     concrete_type.cache_id(generics)
                 }
             }
